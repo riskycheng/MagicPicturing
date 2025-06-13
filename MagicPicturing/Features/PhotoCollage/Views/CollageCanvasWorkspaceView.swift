6950f9f98738267268dfacc3b74fa7b9e6fd5b98
Jian <riskycheng@gmail.com>
2025-06-13 15:30:53 +0800

    fix the position of the upper canvas region

--- MagicPicturing/Features/PhotoCollage/Views/CollageCanvasWorkspaceView.swift
+++ MagicPicturing/Features/PhotoCollage/Views/CollageCanvasWorkspaceView.swift
@@ -19,8 +19,8 @@ struct CollageCanvasWorkspaceView: View {
     }
 
     var body: some View {
-        VStack {
-            ZStack {
+        ZStack {
+            VStack {
                 CanvasView(viewModel: viewModel, canvasSize: canvasSize)
                     .frame(width: canvasSize.width, height: canvasSize.height)
                     .padding(.top, 32)
@@ -35,46 +35,46 @@ struct CollageCanvasWorkspaceView: View {
                                 }
                         }
                     )
-                // 悬浮预览
-                if let draggingImage = draggingImage, let dragPosition = dragPosition, let offset = dragImageOffset, isDragging {
-                    Image(uiImage: draggingImage.image)
-                        .resizable()
-                        .frame(width: 80, height: 80)
-                        .opacity(0.8)
-                        .position(x: dragPosition.x - offset.width, y: dragPosition.y - offset.height)
-                        .zIndex(10)
-                }
-            }
-            Spacer()
-            BottomImagePickerView(
-                images: viewModel.bottomImages,
-                onDragStart: { img, startLocation, imageFrame in
-                    draggingImage = img
-                    dragPosition = startLocation
-                    // 计算手指在图片内的偏移
-                    let offset = CGSize(width: startLocation.x - imageFrame.minX, height: startLocation.y - imageFrame.minY)
-                    dragImageOffset = offset
-                    isDragging = true
-                },
-                onDragUpdate: { location in
-                    dragPosition = location
-                },
-                onDragEnd: { location in
-                    if let img = draggingImage, let pos = location, let offset = dragImageOffset {
-                        // 只有在画布区域内才添加
-                        let dropPoint = CGPoint(x: pos.x - offset.width + 40, y: pos.y - offset.height + 40)
-                        if canvasFrame.contains(dropPoint) {
-                            // 转换为画布内坐标
-                            let canvasPoint = CGPoint(x: dropPoint.x - canvasFrame.minX, y: dropPoint.y - canvasFrame.minY)
-                            viewModel.addImage(at: canvasPoint, image: img, canvasSize: canvasSize)
+                Spacer()
+                BottomImagePickerView(
+                    images: viewModel.bottomImages,
+                    onDragStart: { img, startLocation, imageFrame in
+                        draggingImage = img
+                        dragPosition = startLocation
+                        let offset = CGSize(width: startLocation.x - imageFrame.minX, height: startLocation.y - imageFrame.minY)
+                        dragImageOffset = offset
+                        isDragging = true
+                    },
+                    onDragUpdate: { location in
+                        dragPosition = location
+                    },
+                    onDragEnd: { location in
+                        if let img = draggingImage, let pos = location, let offset = dragImageOffset {
+                            let dropPoint = CGPoint(x: pos.x - offset.width + 40, y: pos.y - offset.height + 40)
+                            if canvasFrame.contains(dropPoint) {
+                                let canvasPoint = CGPoint(x: dropPoint.x - canvasFrame.minX, y: dropPoint.y - canvasFrame.minY)
+                                viewModel.addImage(at: canvasPoint, image: img, canvasSize: canvasSize)
+                            }
                         }
+                        draggingImage = nil
+                        dragPosition = nil
+                        dragImageOffset = nil
+                        isDragging = false
                     }
-                    draggingImage = nil
-                    dragPosition = nil
-                    dragImageOffset = nil
-                    isDragging = false
-                }
-            )
+                )
+            }
+
+            if let draggingImage = draggingImage, let dragPosition = dragPosition, let offset = dragImageOffset, isDragging {
+                Image(uiImage: draggingImage.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 80, height: 80)
+                    .cornerRadius(8)
+                    .opacity(0.8)
+                    .position(x: dragPosition.x - offset.width + 40, y: dragPosition.y - offset.height + 40)
+                    .allowsHitTesting(false)
+                    .zIndex(10)
+            }
         }
         .background(Color.black.edgesIgnoringSafeArea(.all))
     }
